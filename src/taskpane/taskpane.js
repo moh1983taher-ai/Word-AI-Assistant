b583d40f87c06dbaf83acdf2953f01382f44b799
moh1983taher-ai <moh1983taher@gmail.com>
2026-08-29 21:13:04 +0300

    Update new chat sidebar behavior

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -37663,19 +37663,19 @@ if (
                     <span class="scope-option-icon">
 
                         <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="#000000"
-                        stroke-width="1"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="#000000"
+                            stroke-width="1"
+                            stroke-linecap="round"
+                            stroke-linejoin="round">
 
-                        <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
+                            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
 
-                        <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
+                            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
 
-                    </svg>
+                        </svg>
 
                     </span>
 
